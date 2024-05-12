92c969ac0f27f99186d5f4f0143e59e939721713
Michael Musatov <mv.musatov@gmail.com>
2024-05-12 22:07:55 +0400

    Final fixes

--- src/app/_ui/stickty-header.tsx
+++ src/app/_ui/stickty-header.tsx
@@ -1,7 +1,11 @@
 import { FC } from "react";
 
 export const StickyHeader: FC<{ title: string }> = ({ title }) => (
-  <h1 className="w-full text-lg text-center sticky p-4 top-0 bg-slate-100 shadow-b dark:bg-black dark:shadow-slate-500">
-    {title}
-  </h1>
+  <>
+    <h1 className="w-full text-lg -mb-4 z-20 text-center sticky p-4 top-0 bg-slate-100 dark:bg-black">
+      {title}
+    </h1>
+    <div className="w-full sticky top-[52px] h-2 shadow-b dark:shadow-slate-500" />
+    <div className="w-full -mt-4 h-2 z-10 bg-slate-100 dark:bg-black" />
+  </>
 );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { StickyHeader } from "./_ui/stickty-header";
 
 export default function Home() {
   return (
-    <main className="flex gap-4 min-h-screen flex-col items-center justify-between py-24">
+    <main className="flex min-h-screen flex-col items-center justify-between py-24">
       <div className="w-80">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam quos
         quod, doloribus, quidem, tempore quae doloremque voluptates magnam
@@ -11,9 +11,11 @@ export default function Home() {
         voluptas. Quisquam, quibusdam voluptas.
       </div>
       <StickyHeader title="Sticky header" />
-      {Array.from({ length: 10 }, (_, index) => (
-        <Card key={index} index={index} />
-      ))}
+      <div className="mt-4 flex flex-col gap-4">
+        {Array.from({ length: 10 }, (_, index) => (
+          <Card key={index} index={index} />
+        ))}
+      </div>
     </main>
   );
 }
